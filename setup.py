64fc8ee99be73c02dcd47968237b9683ee02c055
ChristosGrigoras <chris.grigoras@gmail.com>
2021-10-20 11:15:57 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,5 +10,5 @@ setup(
     version='0.1',
     license='MIT',
     description='Stock Option Optimizer',
-    long_description=open('README.txt').read(),
+    # long_description=open('README.txt').read(),
 )
