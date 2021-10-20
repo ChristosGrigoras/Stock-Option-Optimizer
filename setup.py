27425f00e812ee240d4de05dd875000fa6d5a442
ChristosGrigoras <chris.grigoras@gmail.com>
2021-10-20 12:15:29 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
 setup(
-    name='StockOptionOptimizer',
-    url='https://github.com/ChristosGrigoras/Stock-Option-Optimizer',
-    author='Christos Grigoras',
-    author_email='chris.grigoras@gmail.com',
-    packages=['sooptimizer'],
-    install_requires=['numpy'],
-    license='MIT',
-    description='Stock Option Optimizer',
-    long_description=open('README.txt').read(),
+#     name='StockOptionOptimizer',
+#     url='https://github.com/ChristosGrigoras/Stock-Option-Optimizer',
+#     author='Christos Grigoras',
+#     author_email='chris.grigoras@gmail.com',
+#     packages=['sooptimizer'],
+#     install_requires=['numpy'],
+#     license='MIT',
+#     description='Stock Option Optimizer',
+#     long_description=open('README.txt').read(),
 )
