9639c8ac47b036daa974d26edbb8240fada9a9b5
ChristosGrigoras <chris.grigoras@gmail.com>
2021-10-20 11:45:10 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,6 @@ setup(
     author_email='chris.grigoras@gmail.com',
     packages=['sooptimizer'],
     install_requires=['numpy'],
-    version='0.1',
     license='MIT',
     description='Stock Option Optimizer',
     long_description=open('README.txt').read(),
